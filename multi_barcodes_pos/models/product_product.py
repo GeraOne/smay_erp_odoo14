95fd1cc80acbe440bb37610add0335745aa0b023
Gerardo <sistemas@supermay.mx>
2021-07-28 12:50:07 -0500

    multiple_barcode

--- multi_barcodes_pos/models/product_product.py
+++ multi_barcodes_pos/models/product_product.py
@@ -60,8 +60,8 @@ class ProductProduct(models.Model):
             return res
         product_ids = self.env['multi.barcode.products'].search([('multi_barcode', '=', name)]).mapped(
             'product_multi.id')
-        if product_ids:
-            return models.lazy_name_get(self.browse(product_ids).with_user(name_get_uid))
+        '''if product_ids:
+            return models.lazy_name_get(self.browse(product_ids).with_user(name_get_uid))'''
         return res
 
 
