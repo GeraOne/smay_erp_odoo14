befe8c7e5e8b989cc874ede41c3612563120452d
Gerardo <sistemas@supermay.mx>
2021-07-29 16:50:44 -0500

    genero

--- grp_module/models/models.py
+++ grp_module/models/models.py
@@ -6,8 +6,18 @@ from odoo import models, fields, api
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    genre = fields.Char(string='Genero')
+    # genre = fields.Char(string='Genero')
+    genre = fields.Selection([('hombre', 'HOMBRE'), ('mujer', 'MUJER'), ], 'Genero', default='mujer')
 
     def change_genre(self):
         for contact in self:
+            new_genre = ''
+            if contact.genre == 'mujer':
+                new_genre = 'hombre'
+            else:
+                new_genre = 'mujer'
+
+            contact.update({
+                'genre': new_genre
+            })
             return True
