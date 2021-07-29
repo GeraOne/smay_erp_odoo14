23bf211533f4c3af8802af785b9b8c39f956ea2d
Gerardo <sistemas@supermay.mx>
2021-07-29 13:14:27 -0500

    tranferences

--- smay_stock_transferences/models/models.py
+++ smay_stock_transferences/models/models.py
@@ -18,7 +18,7 @@ class smayTransferencesResUser(models.Model):
     '''picking_type_id = fields.Many2one('stock.picking.type', string='Tipo de movimiento por default',
                                       domain="[('name','=','Transferencias internas')]")'''
     picking_type_id = fields.Many2one('stock.picking.type', string='Tipo de movimiento por default',
-                                      domain="[('sequence_code','=','INT')]")
+                                      domain="[('sequence_code','ilike','INT')]")
     # domain="[('sequence_code','=','INT')]"
     stock_location_id = fields.Many2one('stock.location', string='Almacen origen para transferencias',
                                         domain="[('barcode','ilike','STOCK')]")
