d93ce2160c968cf6e1a171d385e9c412a9fdc2b0
Gerardo <sistemas@supermay.mx>
2021-07-29 15:42:32 -0500

    genero

--- grp_module/models/models.py
+++ grp_module/models/models.py
@@ -11,17 +11,3 @@ class ResPartner(models.Model):
     def change_genre(self):
         for contact in self:
             return True
-
-# class grp_module(models.Model): contacts.res_partner_menu_contacts
-#     _name = 'grp_module.grp_module'
-#     _description = 'grp_module.grp_module'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
